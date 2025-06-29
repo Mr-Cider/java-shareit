96da18cf9d48c723227e7f0ebbcc4c74156c145d
Levan Harshiladze <leeone95@yandex.ru>
2025-06-29 23:37:17 +0300

    SharIt 3.01 Пытаемся залить на гитхаб

--- gateway/src/test/java/ru/practicum/shareit/ShareItGatewayTest.java
+++ gateway/src/test/java/ru/practicum/shareit/ShareItGatewayTest.java
@@ -5,7 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ShareItGatewayTest {
+
     public static String X_SHARER_USER_ID = "X-Sharer-User-Id";
+
     @Test
     void contextLoads() {
     }
