1f70c96df6d96fb0358dcddcf093f2ba4f2c37b3
Levan Harshiladze <leeone95@yandex.ru>
2025-07-02 22:34:03 +0300

    SharIt 3.03 Работа над ошибками

--- server/src/main/java/ru/practicum/shareit/request/service/RequestServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/request/service/RequestServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.request.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
